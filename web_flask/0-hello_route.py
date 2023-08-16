7b477ee492ac0edb83afdf5ef35b0dd925ecf113
babs <musaabdullahi0398@gmail.com>
2023-08-16 15:11:36 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ 0. Script to start a Flask web application """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+    """ Return a given string """
+    return render_template("10-hbnb_filters.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
